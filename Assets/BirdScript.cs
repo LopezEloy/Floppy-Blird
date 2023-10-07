6a149fb47a4e38fd2709583cde9abd6b1bf348ec
Eloy Lopez <104275355+LopezEloy@users.noreply.github.com>
2023-10-07 01:00:38 -0500

    Created game over screen
    The last component was to have a game over screen and the cause logic.
    First draft of game build

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -7,17 +7,25 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
 
     void Start()
     {
-
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
